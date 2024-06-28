d11852fda3ea82b575854c61f94b44adb44a8864
ShutUp-Pls <marcodelgado.1008@gmail.com>
2024-06-27 22:53:06 -0400

    MultiUtil Obtiene su propio repositorio.
    MultiUtil seguirá su desarrollo en el repositorio "DEV_UtilityHub"

--- DEV_MultiUtil/key_creator.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-def crear_hash(passw):
-    # Crear un hash de la contraseña
-    hash_objeto = hashlib.sha256(passw.encode())
-    hash_hex = hash_objeto.hexdigest()
-    return hash_hex
